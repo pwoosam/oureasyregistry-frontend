4042bd504a8a4a123ea4ea418678b1a2203f4156
Patrick Woo-Sam <pwoosam5@icloud.com>
2018-04-21 22:42:13 -0700

    Added fields and button to send text message

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,23 @@
+import { autoinject } from 'aurelia-dependency-injection';
+import * as $ from "jquery";
+import { PostClient, PhoneMessage } from "./resources/client"
+
+@autoinject()
 export class App {
+  private _apiClient: PostClient;
   message = 'Hello World!';
+  phoneNumber: number;
+  textMessage: string;
+
+  constructor() {
+    this._apiClient = new PostClient('http://localhost:9001');
+  }
+
+  testTexting() {
+    const phoneMessage = new PhoneMessage({
+      phone_number: this.phoneNumber,
+      message: this.textMessage
+    });
+    this._apiClient.sms(phoneMessage);
+  }
 }
